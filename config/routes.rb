91999bdcb39349fbc63825a5c01d432056e69364
Yung Cher Ho <ycho527@hotmail.com>
2020-03-10 13:40:02 +0800

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/destroy'
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+  get 'articles', to: 'articles#index'
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id' 'articles#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
